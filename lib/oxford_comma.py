4a0eea89cc7db638d5c4d132ff6d75bb610ebc97
michaelanbrown <m_brownbrown@yahoo.com>
2024-08-27 10:44:13 -0400

    if length is greater than 2 then we use join

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,3 +1,5 @@
 def oxford_comma(items):
     if len(items) > 1:
-        items[-1] = "and " + items[-1
+        items[-1] = "and " + items[-1]
+    if len(items) > 2:
+        return ', '.join(items)
